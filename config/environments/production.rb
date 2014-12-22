826c951e67f314d4e453185a99e5da2fd82c4f99
sikanrong <sikanrong@gmail.com>
2014-12-22 07:05:15 +0100

    more experimenting with production.rb to make it serve the assets correctly on heroku (un-minified)...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,15 +28,6 @@ Localhost::Application.configure do
   #Enable asset debug mode in production
   config.assets.debug = true
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
